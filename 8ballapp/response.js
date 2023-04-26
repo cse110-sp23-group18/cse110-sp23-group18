aa6541676eb1035a8df1546db0ba037d8f96cb6c
codingsincebirth <imsa442@gmail.com>
2023-04-25 19:18:20 -0700

    added audio

--- 8ballapp/response.js
+++ 8ballapp/response.js
@@ -16,27 +16,51 @@ form.addEventListener('submit', function(e) {
 
 	let response;
     //gives an equal chance to all 10 random answers
+	
 	if (randomNumber < 0.1) {
 		response = 'As sure as the sun rises in the east.';
+		var audio = new Audio("audio/as_sure_as_the.mp3")
+		audio.play();
+
 	} else if (randomNumber < 0.2) {
 		response = 'The winds of fate say yes.';
+		var audio = new Audio("audio/the_winds_of_fate.mp3")
+		audio.play();
 	} else if (randomNumber < 0.3) {
 		response = 'The signs are unclear, ask again later.';
+		var audio = new Audio("audio/the_signs_are_unclear.mp3")
+		audio.play();
 	} else if (randomNumber < 0.4) {
 		response = 'The shadows whisper "no".';
+		var audio = new Audio("audio/the_shadow_whisper_no.mp3")
+		audio.play();
 	} else if (randomNumber < 0.5) {
 		response = 'The mystic realm is shrouded in darkness, and I cannot see the answer clearly.';
+		var audio = new Audio("audio/the_mystic_realm.mp3")
+		audio.play();
 	}  else if (randomNumber < 0.6) {
         response = 'As I gaze into the mystic realm, the answer becomes clear: yes, without a doubt.';
+		var audio = new Audio("audio/as_i_gaze_into.mp3")
+		audio.play();
     }  else if (randomNumber < 0.7) {
         response = 'The omens are not favorable, the answer is no.';
+		var audio = new Audio("audio/the_omens_are_not.mp3")
+		audio.play();
+		
     }   else if (randomNumber < 0.8) {
         response = 'The future is always in motion, but at this moment, the answer is unclear.';
+		var audio = new Audio("audio/the_future_is_always.mp3")
+		audio.play();
     }  else if (randomNumber < 0.9) {
         response = 'The spirits have spoken, and their answer is no.';
+		var audio = new Audio("audio/the_spirits_have_spoken.mp3")
+		audio.play();
     } else {
         response = 'The mystical beings lack the answer.';
+		var audio = new Audio("audio/the_mystical_beings_lack.mp3")
+		audio.play();
     }
+	
 
 	// Add animations to the answer box, still needs plenty fixing
 	answer.innerHTML = '';
