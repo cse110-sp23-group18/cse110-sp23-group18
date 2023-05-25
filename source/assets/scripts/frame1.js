a393c6bdd63fb97868191996f91f3c96599eac55
Jessie Ouyang <jessieyouyang@gmail.com>
2023-05-25 03:20:26 -0700

    update js files for frame1 to add flickering effect

--- source/assets/scripts/frame1.js
+++ source/assets/scripts/frame1.js
@@ -3,3 +3,24 @@
 function init() {}
 
 window.addEventListener('DOMContentLoaded', init);
+
+var oolong = document.getElementById("oolong");
+var matcha = document.getElementById("matcha");
+var green = document.getElementById("green");
+
+
+image.addEventListener("click", function() {
+  flickerImage();
+});
+
+function flickerImage() {
+  var opacity = window.getComputedStyle(image).getPropertyValue("opacity");
+
+  if (opacity === "0") {
+    image.style.opacity = "1";
+  } else {
+    image.style.opacity = "0";
+  }
+
+  setTimeout(flickerImage, 200); // Adjust the delay between flickers (in milliseconds) as desired
+}
