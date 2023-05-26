ac8f5ccd76f12744f3ccc67ca467a7020356c1ef
Jessie Ouyang <jessieyouyang@gmail.com>
2023-05-25 20:22:20 -0700

    create variables to store selected image

--- source/assets/scripts/frame1.js
+++ source/assets/scripts/frame1.js
@@ -1,26 +1,83 @@
 // frame1.js
 
-function init() {}
+function init() {
+  // get image element representing types of tea jars
+var oolong_image = document.getElementById('oolong');
+var matcha_image = document.getElementById('matcha');
+var green_image = document.getElementById('green');
+
+// intialize all chosen value to be false
+var oolong_is_chosen = false;
+var matcha_is_chosen = false;
+var green_is_chosen = false;
+
+// when one image is clicked,
+// if it has not been chosen
+// select current image and deselect other images
+// else deselect current image
+oolong_image.addEventListener('click', function() {
+  if (!oolong_is_chosen) {
+    oolong_is_chosen = true;
+    oolong_image.style.transform = 'scale(1.25)';
+    // set current 
+    oolong_image.classList.add('flicker-effect');
+    // remove other options
+    green_is_chosen = false;
+    green_image.style.transform = 'scale(1)';
+    green_image.classList.remove('flicker-effect');
+    matcha_is_chosen = false;
+    matcha_image.style.transform = 'scale(1)';
+    matcha_image.classList.remove('flicker-effect');
+
+  } else {
+    oolong_is_chosen = false;
+    oolong_image.style.transform = 'scale(1)';
+    oolong_image.classList.remove('flicker-effect');
+  }
+});
+
+matcha_image.addEventListener('click', function() {
+  if (!matcha_is_chosen) {
+    matcha_is_chosen = true;
+    matcha_image.style.transform = 'scale(1.25)';
+    // set current 
+    matcha_image.classList.add('flicker-effect');
+    // remove other options
+    oolong_is_chosen = false;
+    oolong_image.style.transform = 'scale(1)';
+    oolong_image.classList.remove('flicker-effect');
+    green_is_chosen = false;
+    green_image.style.transform = 'scale(1)';
+    green_image.classList.remove('flicker-effect');
+  } else {
+    matcha_is_chosen = false;
+    matcha_image.style.transform = 'scale(1)';
+    matcha_image.classList.remove('flicker-effect');
+  }
+});
+
+green_image.addEventListener('click', function() {
+  if (!green_is_chosen) {
+    green_is_chosen = true;
+    green_image.style.transform = 'scale(1.25)';
+    // set current 
+    green_image.classList.add('flicker-effect');
+    //remove other options
+    oolong_is_chosen = false;
+    oolong_image.style.transform = 'scale(1)';
+    oolong_image.classList.remove('flicker-effect');
+    matcha_is_chosen = false;
+    matcha_image.style.transform = 'scale(1)';
+    matcha_image.classList.remove('flicker-effect');
+  } else {
+    green_is_chosen = false;
+    green_image.style.transform = 'scale(1)';
+    green_image.classList.remove('flicker-effect');
+  }
+});
+
+
+}
 
 window.addEventListener('DOMContentLoaded', init);
 
-var oolong = document.getElementById("oolong");
-var matcha = document.getElementById("matcha");
-var green = document.getElementById("green");
-
-
-image.addEventListener("click", function() {
-  flickerImage();
-});
-
-function flickerImage() {
-  var opacity = window.getComputedStyle(image).getPropertyValue("opacity");
-
-  if (opacity === "0") {
-    image.style.opacity = "1";
-  } else {
-    image.style.opacity = "0";
-  }
-
-  setTimeout(flickerImage, 200); // Adjust the delay between flickers (in milliseconds) as desired
-}
