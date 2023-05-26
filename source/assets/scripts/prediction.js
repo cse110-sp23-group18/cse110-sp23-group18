48b085c6de5f0f136d29437a693ffac0ba7acf68
kNelakonda <knelakon@ucsd.edu>
2023-05-25 19:23:38 -0700

    Added Script to frame 5, in the correct directory
    Also fixed a few bugs

--- source/assets/scripts/prediction.js
+++ source/assets/scripts/prediction.js
@@ -16,43 +16,43 @@ window.addEventListener("DOMContentLoaded", init);
 const PREDICTIONS = [
         {
             "text": "Ah, a fish shape. The tea leaves reveal a wild journey ahead in overseas relations. Success is your destiny!",
-            "picture": "hog.png", 
+            "picture": "./assets/images/dredges/fish1.png", 
         },
         {
             "text": "Oh, the leaves are shaped in a heart, it spill secrets of something amazing coming your way. True love is headed your way!",
-            "picture": "", 
+            "picture": "./assets/images/dredges/heart1.png", 
         },
         {
             "text": "In the tea leaves, a tiny worry in the shape of a bug appears. But fear not, it's just a passing whisper.",
-            "picture": "",
+            "picture": "./assets/images/dredges/bug1.png",
         },
         {
             "text": "With the key shape, I see a readiness for newness. Prepare to welcome new ideas into your life.",
-            "picture": "",
+            "picture": "./assets/images/dredges/key1.png",
         },
         {
             "text": "I see a cloud in the cup. The tea leaves hint at some troubles heading your way soon. But fear not, for challenges are just part of life's wild ride!",
-            "picture": "",
+            "picture": "./assets/images/dredges/cloud1.png",
         },
         {
             "text": "Oh no, the tea leaves appear in the shape of a knife. Warning: fake friends lie in wait.",
-            "picture": "",
+            "picture": "./assets/images/dredges/knife1.png",
         },
         {
             "text": "The leaves looking like a whale reveal a sure thing, a major project holds the guarantee of achievement. Success is on the horizon!",
-            "picture": "",
+            "picture": "./assets/images/dredges/whale1.png",
         },
         {
             "text": "In the flower-shaped tea leaves, a message emerges: someone seeks your advice.",
-            "picture": "",
+            "picture": "./assets/images/dredges/flower1.png",
         },
         {
             "text": "Among us, you have a future that is leagues above the others in importance. Watch over your shoulder in the coming fortnight, because if you are crafty and display much valor, you may reach the apex of your life.",
-            "picture": "",
+            "picture": "./assets/images/dredges/acorn1.png",
         },
         {
             "text": "Tis' with great honor that I get to tell you that your future will shine brighter than the sun as it rises over the hills that surround this valley.",
-            "picture": "",
+            "picture": "./assets/images/dredges/snake1.png",
         }
 ]; 
 
@@ -80,24 +80,24 @@ function init(){
  */
 function prediction(){
     const randomNumber = Math.floor(Math.random() * PREDICTION_COUNT);
-    let prediction = PREDICTIONS[randomNumber].text;
 
-    let predictionEl =  document.querySelector('.prediction');
-    if(predictionEl == null){
-        predictionEl = document.createElement('prediction');
-    }
-    predictionEl.innerHTML = prediction;
+    let prediction = PREDICTIONS[randomNumber].text;
+    let predictionTxt =  document.querySelector('#predictionTxt');
+    // if(predictionEl == null){
+    //     predictionEl = document.createElement('prediction');
+    // }
+    predictionTxt.innerHTML = prediction;
 
     let predictionPic = PREDICTIONS[randomNumber].picture;
-    let predictPicEl = document.querySelector("img");
-    if(predictPicEl == null){
-        predictPicEl = document.createElement("img");
-    }
+    let predictPicEl = document.querySelector("#predictionImg");
+    // if(predictPicEl == null){
+    //     predictPicEl = document.createElement("img");
+    // }
     predictPicEl.src = predictionPic;
 
-    let bodyEl = document.querySelector("body");
-    bodyEl.appendChild(predictionEl);
-    bodyEl.appendChild(predictPicEl);
+    let predictionEl = document.querySelector(".prediction");
+    predictionEl.appendChild(predictionTxt);
+    predictionEl.appendChild(predictPicEl);
 }
 
 
