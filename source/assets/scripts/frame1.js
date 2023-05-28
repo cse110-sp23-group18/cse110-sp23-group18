757853a68ac8a1e9f7b928e889324fe7d5b6c7eb
Grant Cheng <26587113+CatFish47@users.noreply.github.com>
2023-05-28 07:04:23 -0700

    Add basic lint formatting

--- source/assets/scripts/frame1.js
+++ source/assets/scripts/frame1.js
@@ -4,23 +4,22 @@ function init() {}
 
 window.addEventListener('DOMContentLoaded', init);
 
-var oolong = document.getElementById("oolong");
-var matcha = document.getElementById("matcha");
-var green = document.getElementById("green");
+const oolong = document.getElementById('oolong');
+const matcha = document.getElementById('matcha');
+const green = document.getElementById('green');
 
-
-image.addEventListener("click", function() {
-  flickerImage();
+image.addEventListener('click', () => {
+    flickerImage();
 });
 
 function flickerImage() {
-  var opacity = window.getComputedStyle(image).getPropertyValue("opacity");
+    const opacity = window.getComputedStyle(image).getPropertyValue('opacity');
 
-  if (opacity === "0") {
-    image.style.opacity = "1";
-  } else {
-    image.style.opacity = "0";
-  }
+    if (opacity === '0') {
+        image.style.opacity = '1';
+    } else {
+        image.style.opacity = '0';
+    }
 
-  setTimeout(flickerImage, 200); // Adjust the delay between flickers (in milliseconds) as desired
+    setTimeout(flickerImage, 200); // Adjust the delay between flickers (in milliseconds) as desired
 }
