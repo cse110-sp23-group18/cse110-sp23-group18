1ede0465713c8358bcffab24f95e5c3ab7053f56
codingsincebirth <imsa442@gmail.com>
2023-06-01 11:09:42 -0700

    Added unit test for prediction changed somethings

--- source/assets/tests/prediction.test.js
+++ source/assets/tests/prediction.test.js
@@ -1,18 +1,34 @@
-const prediction = require('./source/assets/scripts/prediction.js');
+// Import the necessary modules/functions for testing
+const { prediction, PREDICTIONS } = require('.source/assets/scripts/prediction.js');
+
+// Mock the necessary DOM elements and properties
+document.querySelector = jest.fn((selector) => {
+  if (selector === '#predictionTxt') {
+    return {
+      innerHTML: '',
+    };
+  } else if (selector === '#predictionImg') {
+    return {
+      src: '',
+    };
+  } else if (selector === '.prediction') {
+    return document.createElement('div');
+  }
+});
 
 describe('prediction', () => {
-    it('should update the prediction text and image elements with a random prediction', () => {
-        const randomNumber = 0; // Mocking a specific random number to access a prediction
-        Math.floor = jest.fn(() => randomNumber);
+  test('should update the prediction text and image elements with a random prediction', () => {
+    const randomNumber = 0; // Mocking a specific random number to access a prediction
+    Math.floor = jest.fn(() => randomNumber);
 
-        prediction();
+    prediction();
 
-        // Retrieve the mocked DOM elements
-        const predictionTxtEl = document.querySelector('#predictionTxt');
-        const predictPicEl = document.querySelector('#predictionImg');
+    // Retrieve the mocked DOM elements
+    const predictionTxtEl = document.querySelector('#predictionTxt');
+    const predictPicEl = document.querySelector('#predictionImg');
 
-        // Verify that the prediction text and image elements are updated correctly
-        expect(predictionTxtEl.innerHTML).toBe(PREDICTIONS[randomNumber].text);
-        expect(predictPicEl.src).toBe(PREDICTIONS[randomNumber].picture);
-    });
+    // Verify that the prediction text and image elements are updated correctly
+    expect(predictionTxtEl.innerHTML).toBe(PREDICTIONS[randomNumber].text);
+    expect(predictPicEl.src).toBe(PREDICTIONS[randomNumber].picture);
+  });
 });
