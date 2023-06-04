f1b51d16c9ad150e82f2e4e034785026708a4949
Grant Cheng <26587113+CatFish47@users.noreply.github.com>
2023-06-04 15:03:48 -0700

    Successfully implement unit tests for predicitons

---
+++ jest.setup.js
@@ -0,0 +1,10 @@
+const { JSDOM } = require('jsdom');
+
+const jsdom = new JSDOM('<!doctype html><html><body></body></html>');
+const { window } = jsdom;
+
+global.window = window;
+global.document = window.document;
+global.navigator = {
+    userAgent: 'node.js',
+};
--- source/assets/tests/prediction.test.js
+++ source/assets/tests/prediction.test.js
@@ -1,33 +1,124 @@
-const prediction = require('./source/assets/scripts/prediction.js');
+/**
+ * @jest-environment jsdom
+ */
 
-// Mock the necessary DOM elements and properties
-document.querySelector = jest.fn((selector) => {
-  if (selector === '#predictionTxt') {
-      return {
-      innerHTML: '',
-    };
-  } else if (selector === '#predictionImg') {
-      return {
-      src: '',
-    };
-  } else if (selector === '.prediction') {
-      return document.createElement('div');
-  }
-});
+const {
+    prediction,
+    PREDICTIONS,
+    allFramesReached,
+} = require('../scripts/prediction');
+
+class LocalStorageMock {
+    constructor() {
+        this.store = {};
+    }
+
+    clear() {
+        this.store = {};
+    }
+
+    getItem(key) {
+        return this.store[key] || null;
+    }
+
+    setItem(key, value) {
+        this.store[key] = String(value);
+    }
+
+    removeItem(key) {
+        delete this.store[key];
+    }
+}
 
 describe('prediction', () => {
-  test('should update the prediction text and image elements with a random prediction', () => {
-    const randomNumber = 0; // Mocking a specific random number to access a prediction
-    Math.floor = jest.fn(() => randomNumber);
+    let predictionTxtEl;
+    let predictPicEl;
 
-    prediction();
+    beforeEach(() => {
+        jest.resetModules();
 
-    // Retrieve the mocked DOM elements
-    const predictionTxtEl = document.querySelector('#predictionTxt');
-    const predictPicEl = document.querySelector('#predictionImg');
+        // Mock the necessary global objects
+        global.document = {
+            body: document.createElement('body'),
+            querySelector: jest.fn().mockImplementation((selector) => {
+                if (selector === '#prediction-txt') {
+                    return predictionTxtEl;
+                }
+                if (selector === '#prediction-img') {
+                    return predictPicEl;
+                }
 
-    // Verify that the prediction text and image elements are updated correctly
-    expect(predictionTxtEl.innerHTML).toBe(PREDICTIONS[randomNumber].text);
-    expect(predictPicEl.src).toBe(PREDICTIONS[randomNumber].picture);
-  });
+                return null;
+            }),
+        };
+
+        global.localStorage = new LocalStorageMock();
+
+        document.body.innerHTML = `
+            <div class="prediction">
+            <h2 id="prediction-txt"></h2>
+            <img id="prediction-img" />
+            </div>
+            <a href="index.html" title="Start New Fortune" id="restart"></a>
+        `;
+
+        predictionTxtEl = document.querySelector('#prediction-txt');
+        predictPicEl = document.querySelector('#prediction-img');
+    });
+
+    // test('should not set prediction text and image if all frames are not reached', () => {
+    //     localStorage.setItem('index', 'false'); // Simulate frames not reached
+
+    //     prediction();
+
+    //     expect(predictionTxtEl.innerHTML).toBe('');
+    //     expect(predictPicEl.src).toBe('');
+    // });
+
+    test('should update the prediction text and image elements with a prediction from PREDICTIONS', () => {
+        localStorage.setItem('index', 'true');
+        localStorage.setItem('frame1', 'true');
+        localStorage.setItem('frame2', 'true');
+        localStorage.setItem('frame3', 'true');
+        localStorage.setItem('frame4', 'true');
+
+        prediction();
+
+        // Verify that the prediction text and image elements are updated correctly
+        expect(
+            PREDICTIONS.some((pred) => {
+                const imgSrc = `http://localhost/${pred.picture.slice(2)}`;
+                return (
+                    pred.text === predictionTxtEl.innerHTML &&
+                    imgSrc === predictPicEl.src
+                );
+            })
+        ).toBe(true);
+    });
+});
+
+describe('allFramesReached', () => {
+    test('should return true if all frames are reached', () => {
+        localStorage.setItem('index', 'true');
+        localStorage.setItem('frame1', 'true');
+        localStorage.setItem('frame2', 'true');
+        localStorage.setItem('frame3', 'true');
+        localStorage.setItem('frame4', 'true');
+
+        const result = allFramesReached();
+
+        expect(result).toBe(true);
+    });
+
+    test('should return false if any frame is not reached', () => {
+        localStorage.setItem('index', 'true');
+        localStorage.setItem('frame1', 'true');
+        localStorage.setItem('frame2', 'true');
+        localStorage.setItem('frame3', 'true');
+        localStorage.setItem('frame4', 'false'); // Simulate frame not reached
+
+        const result = allFramesReached();
+
+        expect(result).toBe(false);
+    });
 });
