608a8f68177ca6f45c30226ae8663fb246e46d6a
codingsincebirth <imsa442@gmail.com>
2023-06-11 16:34:51 -0700

    fixed frame2

--- source/assets/tests/frame2.test.js
+++ source/assets/tests/frame2.test.js
@@ -1,59 +1,3 @@
-// import initFrameTwo from '../scripts/frame2.js';
-
-// document.body.innerHTML = `
-//   <div id="frame2-layout">
-//     <img id="teapot" src="" />
-//   </div>
-//   <div id="frame3-layout"></div>
-//   <div id="frame3-template"></div>
-// `;
-
-// jest.mock('../audios/boiling_water.mp3', () => {
-//   return jest.fn().mockImplementation(() => ({
-//     src: '',
-//     preload: '',
-//     currentTime: 0,
-//     play: jest.fn(),
-//     pause: jest.fn()
-//   }));
-// });
-
-// describe('initFrameTwo', () => {
-//   it('should change teapot image and play sound when clicked', () => {
-//     const teapotEle = document.getElementById('teapot');
-//     const boilSound = new Audio();
-//     // boilSound.src = './assets/audios/boiling_water.mp3';
-//     // boilSound.preload = 'auto';
-//     initFrameTwo();
-
-//     // Simulate a click event on the teapot element
-//     teapotEle.click();
-
-//     // Assert that the teapot image is changed to the steaming gif
-//     expect(teapotEle.src).toBe('http://localhost/assets/images/gifs/teapotSteaming.gif');
-
-//     // Assert that the sound's currentTime is set to 0
-//     expect(boilSound.currentTime).toBe(0);
-
-//     // Assert that the sound's play method is called
-//     expect(boilSound.play).toHaveBeenCalled();
-
-//     // Fast-forward 5000 milliseconds to simulate the passage of time
-//     jest.advanceTimersByTime(5000);
-
-//     // Assert that the sound's pause method is called
-//     expect(boilSound.pause).toHaveBeenCalled();
-
-//     // Assert that the layout elements are updated correctly
-//     const thisLayout = document.getElementById('frame2-layout');
-//     const nextLayout = document.getElementById('frame3-layout');
-//     expect(thisLayout.style.display).toBe('none');
-//     expect(thisLayout.innerHTML).toBe('');
-//     expect(nextLayout.innerHTML).toBe('');
-//     expect(nextLayout.style.display).toBe('block');
-//   });
-// });
-
 /**
  * @jest-environment jsdom
  */
@@ -103,9 +47,9 @@ describe('initFrameTwo', () => {
 
     test('should add event listener to teapot element', () => {
         const clickEvent = new MouseEvent('click', {
-            view: window,
-            bubbles: true,
-            cancelable: true,
+            'view': window,
+            'bubbles': true,
+            'cancelable': true
         });
 
         teapotEle.dispatchEvent(clickEvent);
@@ -116,9 +60,9 @@ describe('initFrameTwo', () => {
 
     test('should change layouts after 5000ms', () => {
         const clickEvent = new MouseEvent('click', {
-            view: window,
-            bubbles: true,
-            cancelable: true,
+            'view': window,
+            'bubbles': true,
+            'cancelable': true
         });
 
         teapotEle.dispatchEvent(clickEvent);
