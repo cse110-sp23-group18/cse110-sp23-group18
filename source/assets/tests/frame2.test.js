c05e36c83667252399f0567aa92cdfdae6fe4c35
Grant Cheng <26587113+CatFish47@users.noreply.github.com>
2023-06-11 15:51:48 -0700

    fix lint test js files

--- source/assets/tests/frame2.test.js
+++ source/assets/tests/frame2.test.js
@@ -1,4 +1,4 @@
-import initFrameTwo from '..scripts/frame2.js';
+import initFrameTwo from '../scripts/frame2.js';
 
 document.body.innerHTML = `
   <div id="frame2-layout">
@@ -8,47 +8,47 @@ document.body.innerHTML = `
   <div id="frame3-template"></div>
 `;
 
-jest.mock('html5-audio', () => {
-  return jest.fn().mockImplementation(() => ({
-    src: '',
-    preload: '',
-    currentTime: 0,
-    play: jest.fn(),
-    pause: jest.fn()
-  }));
-});
+jest.mock('html5-audio', () =>
+    jest.fn().mockImplementation(() => ({
+        src: '',
+        preload: '',
+        currentTime: 0,
+        play: jest.fn(),
+        pause: jest.fn(),
+    }))
+);
 
 describe('initFrameTwo', () => {
-  it('should change teapot image and play sound when clicked', () => {
-    const teapotEle = document.getElementById('teapot');
-    const boilSound = new Audio();
+    it('should change teapot image and play sound when clicked', () => {
+        const teapotEle = document.getElementById('teapot');
+        const boilSound = new Audio();
 
-    initFrameTwo();
+        initFrameTwo();
 
-    // Simulate a click event on the teapot element
-    teapotEle.click();
+        // Simulate a click event on the teapot element
+        teapotEle.click();
 
-    // Assert that the teapot image is changed to the steaming gif
-    expect(teapotEle.src).toBe('assets/images/gifs/teapotSteaming.gif');
+        // Assert that the teapot image is changed to the steaming gif
+        expect(teapotEle.src).toBe('assets/images/gifs/teapotSteaming.gif');
 
-    // Assert that the sound's currentTime is set to 0
-    expect(boilSound.currentTime).toBe(0);
+        // Assert that the sound's currentTime is set to 0
+        expect(boilSound.currentTime).toBe(0);
 
-    // Assert that the sound's play method is called
-    expect(boilSound.play).toHaveBeenCalled();
+        // Assert that the sound's play method is called
+        expect(boilSound.play).toHaveBeenCalled();
 
-    // Fast-forward 5000 milliseconds to simulate the passage of time
-    jest.advanceTimersByTime(5000);
+        // Fast-forward 5000 milliseconds to simulate the passage of time
+        jest.advanceTimersByTime(5000);
 
-    // Assert that the sound's pause method is called
-    expect(boilSound.pause).toHaveBeenCalled();
+        // Assert that the sound's pause method is called
+        expect(boilSound.pause).toHaveBeenCalled();
 
-    // Assert that the layout elements are updated correctly
-    const thisLayout = document.getElementById('frame2-layout');
-    const nextLayout = document.getElementById('frame3-layout');
-    expect(thisLayout.style.display).toBe('none');
-    expect(thisLayout.innerHTML).toBe('');
-    expect(nextLayout.innerHTML).toBe('');
-    expect(nextLayout.style.display).toBe('block');
-  });
+        // Assert that the layout elements are updated correctly
+        const thisLayout = document.getElementById('frame2-layout');
+        const nextLayout = document.getElementById('frame3-layout');
+        expect(thisLayout.style.display).toBe('none');
+        expect(thisLayout.innerHTML).toBe('');
+        expect(nextLayout.innerHTML).toBe('');
+        expect(nextLayout.style.display).toBe('block');
+    });
 });
--- source/assets/tests/frame4.test.js
+++ source/assets/tests/frame4.test.js
@@ -1,4 +1,4 @@
-import initFrameFour from '..scripts/frame4.js';
+import initFrameFour from '../scripts/frame4.js';
 
 // Mock the necessary DOM elements and functions
 document.body.innerHTML = `
@@ -31,7 +31,7 @@ describe('initFrameFour', () => {
             preload: '',
             play: mockPlay,
         }));
-        
+
         // Call the function to test
         initFrameFour();
 
@@ -41,50 +41,58 @@ describe('initFrameFour', () => {
 
         // Teacup image should have the 'enlarged' class
         expect(teacupImage.classList.contains('enlarged')).toBe(true);
-        
-        // setTimeout should be called twice  
-        expect(mockSetTimeout).toHaveBeenCalledTimes(2); 
-        
+
+        // setTimeout should be called twice
+        expect(mockSetTimeout).toHaveBeenCalledTimes(2);
+
         // play method of Audio object should be called once
-        expect(mockPlay).toHaveBeenCalledTimes(1);  
-        
+        expect(mockPlay).toHaveBeenCalledTimes(1);
+
         // frame4-layout should have display: none
-        expect(document.getElementById('frame4-layout').style.display).toBe('none');  
-        
+        expect(document.getElementById('frame4-layout').style.display).toBe(
+            'none'
+        );
+
         // frame4-layout should have empty innerHTML
-        expect(document.getElementById('frame4-layout').innerHTML).toBe('');  
-        
+        expect(document.getElementById('frame4-layout').innerHTML).toBe('');
+
         // frame5-layout should have innerHTML equal to 'Frame 5 Template'
-        expect(document.getElementById('frame5-layout').innerHTML).toBe('Frame 5 Template');  
-        
+        expect(document.getElementById('frame5-layout').innerHTML).toBe(
+            'Frame 5 Template'
+        );
+
         // frame5-layout should have display: flex
-        expect(document.getElementById('frame5-layout').style.display).toBe('flex');  
-        
+        expect(document.getElementById('frame5-layout').style.display).toBe(
+            'flex'
+        );
+
         // Audio constructor should be called once
-        expect(mockAudioConstructor).toHaveBeenCalledTimes(1);  
-        
+        expect(mockAudioConstructor).toHaveBeenCalledTimes(1);
+
         // Audio constructor should be called with no arguments
-        expect(mockAudioConstructor).toHaveBeenCalledWith(); 
-        
+        expect(mockAudioConstructor).toHaveBeenCalledWith();
+
         // global.Audio should be called as a constructor
-        expect(global.Audio).toHaveBeenCalledWith();  
-        
+        expect(global.Audio).toHaveBeenCalledWith();
+
         // The src property of the Audio object should be './assets/audios/drinking_tea.mp3'
-        expect(mockAudioConstructor().src).toBe('./assets/audios/drinking_tea.mp3');  
-        
+        expect(mockAudioConstructor().src).toBe(
+            './assets/audios/drinking_tea.mp3'
+        );
+
         // The preload property of the Audio object should be 'auto'
-        expect(mockAudioConstructor().preload).toBe('auto');  
-        
+        expect(mockAudioConstructor().preload).toBe('auto');
+
         // play method of Audio object should be called
-        expect(mockPlay).toHaveBeenCalled();  
-        
+        expect(mockPlay).toHaveBeenCalled();
+
         // setTimeout should be called with a function and a delay of 1500ms
-        expect(mockSetTimeout).toHaveBeenCalledWith(expect.any(Function), 1500); 
-        
+        expect(mockSetTimeout).toHaveBeenCalledWith(expect.any(Function), 1500);
+
         // setTimeout should be called with a function and a delay of 1300ms
-        expect(mockSetTimeout).toHaveBeenCalledWith(expect.any(Function), 1300);  
-        
+        expect(mockSetTimeout).toHaveBeenCalledWith(expect.any(Function), 1300);
+
         // The initFrameFive function from frame5.js should be called
-        expect(require('./frame5.js').default).toHaveBeenCalled();  
+        expect(require('./frame5.js').default).toHaveBeenCalled();
     });
 });
